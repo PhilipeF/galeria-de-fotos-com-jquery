5c928af53860879e92c711b9948f133a7ba495fa
PhilipeF <philipesferreiraa@gmail.com>
2024-02-14 11:19:06 -0300

    Feat: Aplicando o metodo slideDown() e slideUp() do JQuery

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-    console.log(document.querySelector('header button'))
-    console.log($('botao-cancelar'));
-
     $('header button').on('click', () => {
-        alert('Testando esta budega');
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').on('click', () => {
+        $('form').slideUp();
     })
 
     $('form').on('submit', (e) => {
-        console.log('Passei por aqui');
+        console.log('Passei aqui');
         e.preventDefault();
 
     })
